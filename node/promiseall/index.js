67feafb4257a2fe77a10351fbfe0cf172991fa38
YevheniiMaliavka <Yevhenii.Maliavka@mercateo.com>
2018-05-29 21:55:51 +0200

    Implement Promise.all()

--- node/promiseall/index.js
+++ node/promiseall/index.js
@@ -2,6 +2,9 @@
     Target is to implement Promise.all() function
 */
 
+const assert = require('assert');
+const { promiseall } = require('./promiseall');
+
 const sleep = (value, duration) =>
   new Promise(resolve => setTimeout(() => resolve(value)), duration);
 
@@ -10,8 +13,34 @@ const testCaseB = [sleep(5, 500), sleep(6, 100), sleep(7, 0), sleep(8, 1000)];
 const testCaseC = [
   9,
   sleep(10, 1000),
-  new Promise(() => {
-    throw new Error("Promise Error");
+  new Promise(resolve => {
+    throw new Error('Baz!');
   })
 ];
 
+const promiseTests = testCase => {
+  let resultA = null;
+  let resultB = null;
+  Promise.all(testCase)
+    .then(result => {
+      resultA = result;
+      return promiseall(testCase);
+    })
+    .then(result => {
+      resultB = result;
+      assert(resultA);
+      assert(resultB);
+      assert.deepEqual(resultA, resultB);
+      console.log('Test case passed! ', resultA, resultB);
+    })
+    .catch(err => {
+      console.log('An error has occured: ', err.message);
+      console.log(
+        'That means error has been catched by promiseall and execution stopped!'
+      );
+    });
+};
+
+promiseTests(testCaseA);
+promiseTests(testCaseB);
+promiseTests(testCaseC);
