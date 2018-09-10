0afe4bacc5c2c31bac344ed486dfae613bbbed94
Yevhenii <Yevhenii.Maliavka@mercateo.com>
2018-09-10 15:07:01 +0200

    Use styled components

--- react-monorepo/packages/basic-spa-ts/src/index.tsx
+++ react-monorepo/packages/basic-spa-ts/src/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { FancyTitle } from 'styled-component-ts';
+import { FancyTitle, Button } from 'styled-component-ts';
 
 console.log(FancyTitle);
 console.log(require('styled-component-ts'));
-
 
 const FancyApp: React.SFC<{ title: string }> = ({ title }) => (
   <div>
     Test
     <FancyTitle text={title} />
+    <Button dark>FooBar</Button>
   </div>
 );
 
--- react-monorepo/packages/styled-component-ts/src/index.tsx
+++ react-monorepo/packages/styled-component-ts/src/index.tsx
@@ -1,9 +1,51 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 export type FancyTitleProps = {
   text: string;
 };
 
+const fatItalicStyle = (
+  fragments: ReadonlyArray<string>,
+  ...args: string[]
+) => {
+  const firstTag = (
+    <span>
+      {fragments[0]}
+      <b>{args.shift()}</b>
+    </span>
+  );
+  const otherFragments = fragments.slice(1, fragments.length);
+  const others = otherFragments.map((fragment: string, index: number) => (
+    <span>
+      {fragment}
+      <i>{args[index]}</i>
+    </span>
+  ));
+  return (
+    <>
+      {firstTag}
+      {others}
+    </>
+  );
+};
+
+const name = 'My first tagged template string';
+const question = 'Tried styled thingy?';
+const fancyText = fatItalicStyle`Hello ${name}, my question for today: ${question}.`;
+
 export const FancyTitle: React.SFC<FancyTitleProps> = ({ text }) => (
-  <div>{text}</div>
+  <div>{fancyText}</div>
 );
+
+export const Button = styled.button<{ dark?: boolean }>`
+  color: palevioletred;
+  border-radius: 3px;
+  background: transparent;
+
+  ${({ dark }) =>
+    dark &&
+    `
+      background: black;
+    `};
+`;
